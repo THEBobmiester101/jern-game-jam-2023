c6608d33cdf7a5986b18dd7fbcd3dff4e420d60f
TheDangerousCow <dylanrtuttle@gmail.com>
2023-09-15 10:43:49 -0600

    Rough UI
    Created a basic ui for each station. Made a frame work of buttons being able to run a function for tabbing left and right respectively. using component 'GameControl' as the main controls with tracking such as current station

---
+++ Much Assembly Required/Assets/GameMaster.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public List<GameObject> stationPanels;
+    List<float> stationNumber; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void leftStation()
+    {
+
+    }
+
+    public void rightStation()
+    {
+
+    }
+}
