4afee07726cbcd230c1d21fe7ba6f4304d088a11
TheDangerousCow <dylanrtuttle@gmail.com>
2023-09-15 11:07:44 -0600

    Station Tabbing
    Stations can now be tabbed through using the left and right buttons on the panels!

--- Much Assembly Required/Assets/GameMaster.cs
+++ Much Assembly Required/Assets/GameMaster.cs
@@ -4,27 +4,70 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-    public List<GameObject> stationPanels;
-    List<float> stationNumber; 
+    public GameObject[] stationPanels;
+    int curStation;
     // Start is called before the first frame update
     void Start()
     {
-
+        curStation = 0;
+        stationPanels[curStation].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void leftStation()
     {
+        //By pressing the left button, the station tracker moves backwards in the array by one element
 
+        curStation = curStation - 1;
+
+        //If the station count drops below 0, assign it to the last element in the array
+
+        if(curStation < 0)
+        {
+            curStation = 3;
+        }
+
+        //Set all panels to false active in order to ensure theres not 2 panels open (I'd like to make this more efficient later)
+        foreach(GameObject i in stationPanels)
+        {
+            i.SetActive(false);
+        }
+
+
+        //Set the current station panel to active as to be visible
+        stationPanels[curStation].SetActive(true);
+
+        //Thats it!
     }
 
     public void rightStation()
     {
+        //By pressing the right button, the station tracker moves forwards in the array by one element
 
+        curStation = curStation + 1;
+
+        //If the station count goes above 3, assign it to the first element in the array
+
+        if (curStation > 3)
+        {
+            curStation = 0;
+        }
+
+        //Set all panels to false active in order to ensure theres not 2 panels open (I'd like to make this more efficient later)
+        foreach (GameObject i in stationPanels)
+        {
+            i.SetActive(false);
+        }
+
+
+        //Set the current station panel to active as to be visible
+        stationPanels[curStation].SetActive(true);
+
+        //Thats it!
     }
 }
